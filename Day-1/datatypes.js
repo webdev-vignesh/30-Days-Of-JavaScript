c070faa24589dd786dbf16deb24133b727cc5a9b
vignesh murugan <vigmurug@gmail.com>
2023-06-12 18:25:46 +0530

    Create datatypes.js

---
+++ Day-1/datatypes.js
@@ -0,0 +1,32 @@
+let str = "javascript";
+let bool = true;
+const val = undefined;
+let data = null;
+
+// checking typeof variables
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(val));
+console.log(typeof(data));
+
+// declaring 4 variables without assigning values
+let a, b, c, d;
+
+// declaring 4 variables with assigning values
+let i =0, j=1, k=2, l=3;
+
+// variables in multiple lines
+let firstName;
+let lastName;
+let maritalStatus;
+let country;
+let age;
+
+// same variables in single line
+let fName, lName, marital_status, Country, Age;
+
+let myAge = 25;
+let yourAge = 23;
+
+console.log("I am ", myAge, "years old");
+console.log("You are ", yourAge, "years old");
