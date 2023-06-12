17ce68fd44aebad164dffaf92f1321fb317ad673
vignesh murugan <vigmurug@gmail.com>
2023-06-12 17:31:05 +0530

    Create variable.js

---
+++ Day-1/variable.js
@@ -0,0 +1,11 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable, 
+easy to reuse and informative */
+
+let str = "javascript";
+let bool = true;
+const val = undefined;
+let data = null;
